ab5a2bf75af318dbeef84dadef96be860c8e0712
sprintf <paul@normingtons.org>
2022-04-21 20:11:51 -0700

    adds a github action that compiles all the files into a jar.
    moves the build into a local gradle build for these files

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
+
+val protobufVersion = "3.19.1"
+val protobufPluginVersion = "0.8.18"
+// setting this back as lemon-pi can't go newer than this
+val grpcVersion = "1.43.2"
+
+plugins {
+    idea
+    `java-library`
+    kotlin("jvm") version "1.5.31"
+    id("com.google.protobuf") version "0.8.18"
+}
+
+group = "com.normtronix"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("io.grpc:grpc-protobuf:${grpcVersion}")
+    implementation("io.grpc:grpc-stub:${grpcVersion}")
+    implementation("io.grpc:grpc-kotlin-stub:0.1.5")
+    compileOnly("jakarta.annotation:jakarta.annotation-api:1.3.5") // Java 9+ compatibility - Do NOT update to 2.0.0
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:${protobufVersion}"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:${grpcVersion}"
+        }
+        id ("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:0.1.5"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+        }
+    }
+}
+
